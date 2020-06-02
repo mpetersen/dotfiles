ce2a99db87b2ecae44e0080735701a62e1b2eca2
Moritz <mpeterse@adobe.com>
2020-06-02 21:27:56 +0200

    Removed all apps from dock by default.

--- dock.sh
+++ dock.sh
@@ -4,14 +4,6 @@
 dockutil --remove all --no-restart
 
 apps=(
-	"/System/Applications/Utilities/Console.app"
-	"/Applications/iTerm.app"
-	"/System/Applications/Contacts.app"
-	"/System/Applications/Mail.app"
-	"/System/Applications/Messages.app"
-	"/Applications/Safari.app"
-	"/System/Applications/Calendar.app"
-	"/Applications/Microsoft OneNote.app"
 )
 
 for app in "${apps[@]}"; do
